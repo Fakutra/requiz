e3765719fb1c5cb2780be7030ac8f88a08c6bee5
Bagas Prasetio <bagas.prasetio@iconpln.co.id>
2025-07-08 09:53:12 +0700

    update 08/07/2025

--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified', 'role:user'])->name('dashboard');
+})->name('dashboard');
+
+Route::get('/job/detail', function(){
+    return view('jobdetail');
+})->name('jobdetail');
+
+Route::get('/job/applied', function(){
+    return view('client/applied');
+})->name('jobapplied');
+
+Route::get('/userprofile', function(){
+    return view('client/profile');
+})->name('userprofile');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
-    Route::get('lowongan', [LowonganController::class, 'index'])->name('lowongan.index');
+    Route::get('/lowongan', [LowonganController::class, 'index'])->name('lowongan.index');
     Route::get('apply/{id}', [LowonganController::class, 'create'])->name('apply.create');
 
 });
