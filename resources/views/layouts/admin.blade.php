f68d1ea3a34c93900cd0501eaa95f87093e191e8
Fauzan Dinan Muhammad <fauzan.dinan@iconpln.co.id>
2025-10-30 14:09:53 +0700

    benerin yg ga bener

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,17 +19,20 @@
     {{-- Vite --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+    {{-- Bootstrap & Icons --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
     {{-- Trix --}}
     <link rel="stylesheet" href="https://unpkg.com/trix@2.0.8/dist/trix.css">
     <script src="https://unpkg.com/trix@2.0.8/dist/trix.umd.min.js" defer></script>
 
-    {{-- AlpineJS --}}
+    {{-- AlpineJS (cukup sekali) --}}
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-    {{-- ApexCharts --}}
+    {{-- ApexCharts (opsional) --}}
     <script defer src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     {{-- Styles khusus halaman tertentu --}}
     @stack('styles')
