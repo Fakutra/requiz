aa6d67da8815664d92dad1380c9bea809ddaec4c
Fauzan Dinan Muhammad <fauzan.dinan@iconpln.co.id>
2026-01-05 15:03:53 +0700

    button panah selection

--- resources/views/admin/applicant/seleksi/administrasi/index.blade.php
+++ resources/views/admin/applicant/seleksi/administrasi/index.blade.php
@@ -9,6 +9,12 @@
       </a>
 
       <h2 class="text-lg font-semibold leading-none m-0">Seleksi Administrasi</h2>
+      <a href="{{ route('admin.applicant.seleksi.tes_tulis.index',['batch'=>$batchId]) }}" 
+        class="ml-auto text-gray-600 hover:text-gray-900 flex items-center">
+        <svg class="w-6 h-6 flex-shrink-0" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+        </svg>
+      </a>
     </div>
 
     {{-- Toolbar --}}
--- resources/views/admin/applicant/seleksi/technical-test/index.blade.php
+++ resources/views/admin/applicant/seleksi/technical-test/index.blade.php
@@ -9,6 +9,25 @@
       </a>
 
       <h2 class="text-lg font-semibold leading-none m-0">Seleksi Technical Test</h2>
+      <div class="ml-auto flex items-center">
+
+        <!-- Panah kiri (nempel) -->
+        <a href="{{ route('admin.applicant.seleksi.tes_tulis.index',['batch'=>$batchId]) }}" 
+          class="text-gray-600 hover:text-gray-900 flex items-center">
+          <svg class="w-6 h-6 flex-shrink-0" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+          </svg>
+        </a>
+
+        <!-- Panah kanan -->
+        <a href="{{ route('admin.applicant.seleksi.interview.index',['batch'=>$batchId]) }}" 
+          class="text-gray-600 hover:text-gray-900 flex items-center">
+          <svg class="w-6 h-6 flex-shrink-0" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+          </svg>
+        </a>
+
+      </div>
     </div>
 
     {{-- Toolbar --}}
